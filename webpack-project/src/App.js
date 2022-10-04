4af3f1272c03a6f166d671c742e99962039534e2
luzede <85195923+luzede@users.noreply.github.com>
2022-10-04 20:21:33 +0300

    tests

--- webpack-project/src/App.js
+++ webpack-project/src/App.js
@@ -1,9 +1,25 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect} from 'react'
+import axios from 'axios'
 import './index.css'
+
+const useNotes = (url) => {
+  const [notes, setNotes] = useState([])
+  
+  useEffect(() => {
+    axios.get(url).then((res) => {
+      setNotes(res.data)
+    })
+  }, [url])
+  
+  return notes
+}
 
 const App = () => {
   const [counter, setCounter] = useState(0)
   const [values, setValues] = useState([])
+
+  const url = BACKEND_URL
+  const notes = useNotes(url)
 
   const handleClick = () => {
     setCounter(counter + 1)
@@ -16,6 +32,7 @@ const App = () => {
       <button onClick={handleClick}>
         press
       </button>
+      <div>{notes.length} notes on server {url}</div>
     </div>
   )
 }
--- webpack-project/webpack.config.js
+++ webpack-project/webpack.config.js
@@ -1,32 +1,47 @@
 const path = require('path')
+const webpack = require('webpack')
 
-const config = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'main.js',
-  },
-  devServer: {
-    static: path.resolve(__dirname, 'build'),
-    compress: true,
-    port: 3000,
-  },
-  devtool: 'source-map',
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-react', '@babel/preset-env']
+const config = (env, argv) => {
+  console.log('argv', argv.mode);
+
+  const backend_url = argv.mode === 'production'
+    ? 'https://obscure-harbor-49797.herokuapp.com/api/notes'
+    : 'https://obscure-harbor-49797.herokuapp.com/api/notes'
+
+  return {
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'build'),
+      filename: 'main.js',
+    },
+    devServer: {
+      static: path.resolve(__dirname, 'build'),
+      compress: true,
+      port: 3000,
+    },
+    devtool: 'source-map',
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env']
+          }
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader']
         }
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      }
+      ]
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        BACKEND_URL: JSON.stringify(backend_url)
+      })
     ]
   }
+
 }
 
 module.exports = config

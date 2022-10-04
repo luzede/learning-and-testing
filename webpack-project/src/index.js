64efc36ef727fc317e153d590d9d4295103ba053
luzede <85195923+luzede@users.noreply.github.com>
2022-10-04 19:21:57 +0300

    Tested setting up react app from scratch without using create-react-app

---
+++ webpack-project/src/index.js
@@ -0,0 +1,10 @@
+import 'core-js/stable/index.js' //FOR ASYNC/AWAIT TO WORK
+import 'regenerator-runtime/runtime.js' //FOR ASYNC/AWAIT TO WORK
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
